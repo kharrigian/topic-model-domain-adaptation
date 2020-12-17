5480a74d4a9c415dada9867a2080f0f46d415849
Keith Harrigian <keithharrigian@gmail.com>
2020-12-17 14:41:08 -0500

    Stash config

--- scripts/model/score.py
+++ scripts/model/score.py
@@ -5,13 +5,19 @@
 
 ## Choose Directories to Analyze
 COMPARE_DIRS = {
-    "LDA":"./data/results/depression/sample_size/clpsych_wolohan/LDA/",
-    "PLDA":"./data/results/depression/sample_size/clpsych_wolohan/PLDA/",
+    "LDA":"./data/results/depression/k_latent/wolohan_clpsych/LDA/",
+    "PLDA":"./data/results/depression/k_latent/wolohan_clpsych/PLDA/",
 }
+
+## Plot Directory
+PLOT_DIR = "./plots/classification/wolohan_clpsych/"
+
+## Analysis Type
+ANALYSIS_TYPE = "k_latent" # "sample_size", "prior", "k_latent"
 
 ## Metrics
 METRIC_OPT = "f1"
-METRIC_VARS = ["f1","precision","recall","avg_precision","auc"]
+METRIC_VARS = ["f1","precision","avg_precision","auc"]
 
 ########################
 ### Imports
@@ -89,118 +95,136 @@ scores_df = pd.concat(scores_df).reset_index(drop=True)
 ### Plot Results
 ########################
 
+## Establish Analysis-specific Plot Directory
+ad = f"{PLOT_DIR}/{ANALYSIS_TYPE}/".replace("//","/")
+if not os.path.exists(ad):
+    _ = os.makedirs(ad)
+
 ######## Sample Size
 
-group = "development"
-domain = "target"
-metric = "f1"
-
-sample_size = pd.pivot_table(scores_df.loc[(scores_df["domain"]==domain)&(scores_df["group"]==group)],
-                             index=["target_sample_size"],
-                             columns=["experiment","source_sample_size"],
-                             values=metric,
-                             aggfunc=np.mean)
-
-lda_unstack = sample_size["LDA"].iloc[::-1]
-plda_unstack = sample_size["PLDA"].iloc[::-1]
-
-fig, ax = plt.subplots(1,2,figsize=(15,5), sharey=True)
-for i, (u, cmap) in enumerate(zip([lda_unstack, plda_unstack],[plt.cm.Blues,plt.cm.Reds])):
-    m = ax[i].imshow(u, cmap=cmap, aspect="auto", alpha=0.5, vmin=sample_size.min().min(), vmax=sample_size.max().max())
-    ax[i].set_xticks(range(u.shape[1]))
-    ax[i].set_xticklabels(["{:.0f}%".format(int(float("{:.1f}".format(i))*100)) for i in u.columns / u.columns.max()])
-    ax[i].set_yticks(range(u.shape[0]))
-    ax[i].set_yticklabels(["{:.0f}%".format(int(float("{:.1f}".format(i))*100)) for i in u.index / u.index.max()])
-    for k, row in enumerate(u.values):
-        for j, col in enumerate(row):
-            if pd.isnull(col):
-                continue
-            ax[i].text(j, k, "{:.3f}".format(col)[1:], ha="center", va="center", color="black", fontsize=12, fontweight="bold" if np.isclose(col, np.nanmax(u.values)) else "normal")
-    ax[i].set_xlabel("Source Sample Size", fontsize=18, fontweight="bold")
-    if i == 0:
-        ax[i].set_ylabel("Target Sample Size", fontsize=18, fontweight="bold")
-ax[0].set_title("LDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
-ax[1].set_title("PLDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
-for a in ax:
-    a.tick_params(labelsize=14)
-fig.tight_layout()
-plt.show()
+if ANALYSIS_TYPE == "sample_size":
+    ## Cycle Through Combos
+    for group in ["train","development"]:
+        for domain in ["source","target"]:
+            for metric in METRIC_VARS:
+                ## Get Data
+                sample_size = pd.pivot_table(scores_df.loc[(scores_df["domain"]==domain)&(scores_df["group"]==group)],
+                                            index=["target_sample_size"],
+                                            columns=["experiment","source_sample_size"],
+                                            values=metric,
+                                            aggfunc=np.mean)
+                ## Separate Models
+                lda_unstack = sample_size["LDA"].iloc[::-1]
+                plda_unstack = sample_size["PLDA"].iloc[::-1]
+                plda_unstack[0] = np.nan; plda_unstack = plda_unstack[sorted(plda_unstack.columns)]
+                ## Generate Figure
+                fig, ax = plt.subplots(1,2,figsize=(15,5), sharey=True)
+                for i, (u, cmap) in enumerate(zip([lda_unstack, plda_unstack],[plt.cm.Blues,plt.cm.Reds])):
+                    m = ax[i].imshow(u, cmap=cmap, aspect="auto", alpha=0.5)#, vmin=sample_size.min().min(), vmax=sample_size.max().max())
+                    ax[i].set_xticks(range(u.shape[1]))
+                    ax[i].set_xticklabels(["{:.0f}%".format(int(float("{:.1f}".format(i))*100)) for i in u.columns / u.columns.max()])
+                    ax[i].set_yticks(range(u.shape[0]))
+                    ax[i].set_yticklabels(["{:.0f}%".format(int(float("{:.1f}".format(i))*100)) for i in u.index / u.index.max()])
+                    for k, row in enumerate(u.values):
+                        for j, col in enumerate(row):
+                            if pd.isnull(col):
+                                ax[i].text(j, k, "-", fontsize=12, ha="center", va="center")
+                                continue
+                            ax[i].text(j, k, "{:.3f}".format(col)[1:], ha="center", va="center", color="black", fontsize=12, fontweight="bold" if np.isclose(col, np.nanmax(u.values)) else "normal")
+                    ax[i].set_xlabel("Source Sample Size", fontsize=18, fontweight="bold")
+                    if i == 0:
+                        ax[i].set_ylabel("Target Sample Size", fontsize=18, fontweight="bold")
+                ax[0].set_title("LDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
+                ax[1].set_title("PLDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
+                for a in ax:
+                    a.tick_params(labelsize=14)
+                fig.tight_layout()
+                fig.savefig(f"{ad}{group}_{domain}_{metric}.pdf")
+                plt.close(fig)
 
 ######## Prior
 
-# group = "development"
-# domain = "target"
-# metric = "f1"
+if ANALYSIS_TYPE == "prior":
+    ## Cycle Through Combos
+    for group in ["train","development"]:
+        for domain in ["source","target"]:
+            for metric in METRIC_VARS:
+                ## Get Scores
+                prior = pd.pivot_table(scores_df.loc[(scores_df["domain"]==domain)&(scores_df["group"]==group)],
+                               index=["alpha","beta"],
+                               columns=["experiment"],
+                               values=metric,
+                               aggfunc=np.mean)
+                ## Separate Models
+                lda_unstack = prior.unstack()["LDA"].iloc[::-1]
+                plda_unstack = prior.unstack()["PLDA"].iloc[::-1]
+                ## Generate Plot
+                fig, ax = plt.subplots(1,2,figsize=(15,5),sharey=True)
+                for i, (u, cmap) in enumerate(zip([lda_unstack, plda_unstack],[plt.cm.Blues,plt.cm.Reds])):
+                    m = ax[i].imshow(u, cmap=cmap, aspect="auto", alpha=0.5, vmin=prior.min().min(), vmax=prior.max().max())
+                    ax[i].set_xticks( range(u.shape[1]))
+                    ax[i].set_xticklabels(u.columns)
+                    ax[i].set_yticks(range(u.shape[0]))
+                    ax[i].set_yticklabels(u.index)
+                    for k, row in enumerate(u.values):
+                        for j, col in enumerate(row):
+                            if pd.isnull(col):
+                                ax[i].text(j, k, "-", fontsize=12, ha="center", va="center")
+                                continue
+                            ax[i].text(j, k, "{:.3f}".format(col)[1:] if col != 1 else 1, ha="center", va="center", color="black", fontsize=12, fontweight="bold" if col == np.nanmax(u.values) else "normal")
+                    ax[i].set_xlabel("$\\beta$", fontsize=18, fontweight="bold")
+                    if i == 0:
+                        ax[i].set_ylabel("$\\alpha$", fontsize=18, fontweight="bold", rotation=0, labelpad=20)
+                ax[0].set_title("LDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
+                ax[1].set_title("PLDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
+                for a in ax:
+                    a.tick_params(labelsize=14)
+                fig.tight_layout()
+                fig.savefig(f"{ad}{group}_{domain}_{metric}.pdf")
+                plt.close(fig)
 
-# prior = pd.pivot_table(scores_df.loc[(scores_df["domain"]==domain)&(scores_df["group"]==group)],
-#                index=["alpha","beta"],
-#                columns=["experiment"],
-#                values=metric,
-#                aggfunc=np.mean)
-
-# lda_unstack = prior.unstack()["LDA"].iloc[::-1]
-# plda_unstack = prior.unstack()["PLDA"].iloc[::-1]
-
-# fig, ax = plt.subplots(1,2,figsize=(15,5),sharey=True)
-# for i, (u, cmap) in enumerate(zip([lda_unstack, plda_unstack],[plt.cm.Blues,plt.cm.Reds])):
-#     m = ax[i].imshow(u, cmap=cmap, aspect="auto", alpha=0.5, vmin=prior.min().min(), vmax=prior.max().max())
-#     ax[i].set_xticks( range(u.shape[1]))
-#     ax[i].set_xticklabels(u.columns)
-#     ax[i].set_yticks(range(u.shape[0]))
-#     ax[i].set_yticklabels(u.index)
-#     for k, row in enumerate(u.values):
-#         for j, col in enumerate(row):
-#             if pd.isnull(col):
-#                 continue
-#             ax[i].text(j, k, "{:.3f}".format(col)[1:], ha="center", va="center", color="black", fontsize=12, fontweight="bold" if col == np.nanmax(u.values) else "normal")
-#     ax[i].set_xlabel("$\\beta$", fontsize=18, fontweight="bold")
-#     if i == 0:
-#         ax[i].set_ylabel("$\\alpha$", fontsize=18, fontweight="bold", rotation=0, labelpad=20)
-# ax[0].set_title("LDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
-# ax[1].set_title("PLDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
-# for a in ax:
-#     a.tick_params(labelsize=14)
-# fig.tight_layout()
-# plt.show()
 
 ######## Latent Topics
 
-# group = "development"
-# domain = "target"
-# metric = "f1"
-
-# latent = pd.pivot_table(scores_df.loc[(scores_df["domain"]==domain)&(scores_df["group"]==group)],
-#                index=["k_latent","k_per_label"],
-#                columns=["experiment"],
-#                values=metric,
-#                aggfunc=np.mean)
-
-# latent_lda = latent["LDA"].dropna()
-# latent_plda_unstack = latent["PLDA"].unstack().T.iloc[::-1]
-
-# fig, ax = plt.subplots(1,2,figsize=(15,5))
-# ax[0].plot(latent_lda.index.levels[0], latent_lda.values, marker="o", linestyle="--", alpha=0.8, linewidth=3, markersize=10)
-# ax[0].axhline(latent_plda_unstack.max().max(), linestyle="--", alpha=0.8, color="darkred", label="PLDA Max Score", linewidth=3)
-# m = ax[1].imshow(latent_plda_unstack, cmap=plt.cm.Reds, aspect="auto", alpha=0.5)
-# cbar = fig.colorbar(m, ax=ax[1])
-# cbar.ax.tick_params(labelsize=14)
-# ax[1].set_xticks( range(latent_plda_unstack.shape[1]))
-# ax[1].set_xticklabels(latent_plda_unstack.columns)
-# ax[1].set_yticks(range(latent_plda_unstack.shape[0]))
-# ax[1].set_yticklabels(latent_plda_unstack.index)
-# for i, row in enumerate(latent_plda_unstack.values):
-#     for j, col in enumerate(row):
-#         ax[1].text(j, i, "{:.3f}".format(col)[1:], ha="center", va="center", color="black", fontsize=12)
-# ax[0].set_xlabel("# Latent Topics", fontsize=18, fontweight="bold")
-# ax[0].set_ylabel(metric.replace("_"," ").title() if len(metric) > 3 else metric.upper(), fontsize=18, fontweight="bold")
-# ax[1].set_xlabel("# Latent Topics", fontsize=18, fontweight="bold")
-# ax[1].set_ylabel("# Topics\nPer Domain", fontsize=18, fontweight="bold")
-# ax[0].spines["top"].set_visible(False)
-# ax[0].spines["right"].set_visible(False)
-# ax[0].legend(loc="lower right", fontsize=14)
-# ax[0].set_title("LDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
-# ax[1].set_title("PLDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
-# for a in ax:
-#     a.tick_params(labelsize=14)
-# fig.tight_layout()
-# plt.show()
+if ANALYSIS_TYPE == "k_latent":
+    ## Cycle Through Combos
+    for group in ["train","development"]:
+        for domain in ["source","target"]:
+            for metric in METRIC_VARS:
+                ## Get Score
+                latent = pd.pivot_table(scores_df.loc[(scores_df["domain"]==domain)&(scores_df["group"]==group)],
+                            index=["k_latent","k_per_label"],
+                            columns=["experiment"],
+                            values=metric,
+                            aggfunc=np.mean)
+                ## Serparate Results
+                latent_lda = latent["LDA"].dropna()
+                latent_plda_unstack = latent["PLDA"].unstack().T.iloc[::-1]
+                ## Generate Plots
+                fig, ax = plt.subplots(1,2,figsize=(15,5))
+                ax[0].plot(latent_lda.index.levels[0], latent_lda.values, marker="o", linestyle="--", alpha=0.8, linewidth=3, markersize=10)
+                ax[0].axhline(latent_plda_unstack.max().max(), linestyle="--", alpha=0.8, color="darkred", label="PLDA Max Score", linewidth=3)
+                m = ax[1].imshow(latent_plda_unstack, cmap=plt.cm.Reds, aspect="auto", alpha=0.5)
+                cbar = fig.colorbar(m, ax=ax[1])
+                cbar.ax.tick_params(labelsize=14)
+                ax[1].set_xticks( range(latent_plda_unstack.shape[1]))
+                ax[1].set_xticklabels(latent_plda_unstack.columns)
+                ax[1].set_yticks(range(latent_plda_unstack.shape[0]))
+                ax[1].set_yticklabels(latent_plda_unstack.index)
+                for i, row in enumerate(latent_plda_unstack.values):
+                    for j, col in enumerate(row):
+                        ax[1].text(j, i, "{:.3f}".format(col)[1:], ha="center", va="center", color="black", fontsize=12, fontweight="bold" if col == np.nanmax(latent_plda_unstack.values) else "normal")
+                ax[0].set_xlabel("# Latent Topics", fontsize=18, fontweight="bold") 
+                ax[0].set_ylabel(metric.replace("_"," ").title() if len(metric) > 3 else metric.upper(), fontsize=18, fontweight="bold")
+                ax[1].set_xlabel("# Latent Topics", fontsize=18, fontweight="bold")
+                ax[1].set_ylabel("# Topics\nPer Domain", fontsize=18, fontweight="bold")
+                ax[0].spines["top"].set_visible(False)
+                ax[0].spines["right"].set_visible(False)
+                ax[0].legend(loc="lower right", fontsize=14)
+                ax[0].set_title("LDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
+                ax[1].set_title("PLDA", loc="left", fontweight="bold", fontstyle="italic", fontsize=20)
+                for a in ax:
+                    a.tick_params(labelsize=14)
+                fig.tight_layout()
+                fig.savefig(f"{ad}{group}_{domain}_{metric}.pdf")
+                plt.close(fig)
